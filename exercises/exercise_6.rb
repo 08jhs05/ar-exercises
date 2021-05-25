90de03b1431925171337ac12e992878059203b86
08jhs05 <08jhs05@gmail.com>
2021-05-25 05:01:25 +0000

    done exercise 1-7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Pekora", last_name: "Usada", hourly_rate: 100)
+@store2.employees.create(first_name: "Kizuna", last_name: "Ai", hourly_rate: 20)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,28 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee < ActiveRecord::Base
+  validates :first_name, :last_name, :store_id, presence: true
+  validates :hourly_rate, numericality: { greater_than: 40, less_than: 200 }
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :must_carry_atleast_mens_or_womens
+
+  def must_carry_atleast_mens_or_womens
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
+
+end
+
+puts "enter a name for new store"
+newstorename = gets.chomp
+store = Store.create(name: newstorename)
+puts Store.create.errors[:name]
+puts Store.create.errors[:annual_revenue]
+puts Store.create.errors[:mens_apparel]
